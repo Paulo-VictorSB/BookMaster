0316391ad69799e7ce2bbaedc4b75daaaa2b45de
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-28 10:33:29 -0300

    Models e Migrations implementadas

---
+++ BookMasterApi/app/Models/Book.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title',
+        'isbn',
+        'publisher_id',
+        'release_year',
+        'description'
+    ];
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+}
