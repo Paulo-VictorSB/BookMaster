462dbb33b6e6ff49193ab9547e73f8e5c7ade3b1
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-30 10:25:35 -0300

    Método para adicionar um livro implementado, utilizando o Request e o Response para gerar respostas padronizadas, se já existir um autor, categoria ou publicadora ele nem implementa uma nova, apenas pega o id e faz toda a regra de negócios.

--- BookMasterApi/app/Http/Controllers/BookController.php
+++ BookMasterApi/app/Http/Controllers/BookController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBookRequest;
 use App\Http\Response\Response;
+use App\Models\Author;
 use App\Models\Book;
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Publisher;
 
 class BookController extends Controller
 {
@@ -13,5 +16,54 @@ class BookController extends Controller
         $books = Book::all();
 
         return (new Response)->setData($books)->response();
+    }
+
+    public function store(StoreBookRequest $request)
+    {
+        $response = new Response();
+
+        try {
+            $validated = $request->validated();
+
+            $author = Author::firstOrCreate(
+                ['name' => $validated['author']],
+                [
+                    'birthdate' => $validated['authorBirthdate'],
+                    'bio' => $validated['authorBio']
+                ]
+            );
+
+            $publisher = Publisher::firstOrCreate(
+                ['name' => $validated['publisher']],
+                ['country' => $validated['publisherCountry'] ?? null]
+            );
+
+            $category = Category::firstOrCreate(
+                ['name' => $validated['category']]
+            );
+
+            $book = Book::create([
+                'title' => $validated['title'],
+                'isbn' => $validated['isbn'],
+                'publisher_id' => $publisher->id,
+                'release_year' => $validated['releaseYear'],
+                'description' => $validated['description']
+            ]);
+
+            $book->authors()->attach($author->id);
+            $book->categories()->attach($category->id);
+
+            return $response
+                ->setCode(200)
+                ->setMessage('Livro criado com sucesso')
+                ->setData($book)
+                ->response();
+        } catch (\Exception $e) {
+            return $response
+                ->setStatus('error')
+                ->setCode(500)
+                ->setErrorMessage('Erro ao criar livro' . $e->getMessage())
+                ->response();
+        }
     }
 }
