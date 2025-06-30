d5297b7675c78e40be39da7a04c78c514dc3ef1d
Paulo Victor <paulovdbarbosa@gmail.com>
2025-06-30 16:42:18 -0300

    Rota para atualizar dados de um livro.

--- BookMasterApi/routes/api.php
+++ BookMasterApi/routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('book/list', [BookController::class, 'list']);
 Route::post('book/store', [BookController::class, 'store']);
+Route::put('book/update', [BookController::class, 'update']);
