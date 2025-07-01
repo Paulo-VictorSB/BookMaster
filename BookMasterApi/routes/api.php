e6d9c492f6ab9e84e31dfc7f2f525809227655c9
Paulo Victor <paulovdbarbosa@gmail.com>
2025-07-01 11:08:27 -0300

    Iniciando a parte das rotas publishers

--- BookMasterApi/routes/api.php
+++ BookMasterApi/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('book')->group(function () {
@@ -16,4 +17,11 @@ Route::prefix('author')->group(function () {
     Route::post('/store', [AuthorController::class, 'store']);
     Route::put('/update', [AuthorController::class, 'update']);
     Route::delete('/delete', [AuthorController::class, 'delete']);
+});
+
+Route::prefix('publisher')->group(function () {
+    Route::get('/list', [PublisherController::class, 'list']);
+    Route::post('/store', [PublisherController::class, 'store']);
+    Route::put('/update', [PublisherController::class, 'update']);
+    Route::delete('/delete', [PublisherController::class, 'delete']);
 });
