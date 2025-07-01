e059f3f5b5e4e322fa584dd65a009113fa6572c5
Paulo Victor <paulovdbarbosa@gmail.com>
2025-07-01 07:47:54 -0300

    Rota para deletar um livro

--- BookMasterApi/routes/api.php
+++ BookMasterApi/routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('book/list', [BookController::class, 'list']);
 Route::post('book/store', [BookController::class, 'store']);
 Route::put('book/update', [BookController::class, 'update']);
+Route::delete('book/delete', [BookController::class, 'delete']);
