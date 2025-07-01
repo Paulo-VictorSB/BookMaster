8a1bb1805932e706fa0f009aee180ea9716a8a5a
Paulo Victor <paulovdbarbosa@gmail.com>
2025-07-01 11:48:41 -0300

    Validação do ID para deletar uma publicadora

---
+++ BookMasterApi/app/Http/Requests/DeletePublisherRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Response\Response;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class DeletePublisherRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|integer',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id.required' => 'Você precisa informar um ID para deletar o livro.',
+            'id.integer' => 'O ID precisa ser um inteiro'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $response = new Response();
+
+        throw new HttpResponseException(
+            $response
+                ->setStatus('error')
+                ->setCode(422)
+                ->setErrorMessage('Erro de validação dos campos.')
+                ->setAditionalFields($validator->errors(), 'errors')
+                ->response()
+        );
+    }
+}
